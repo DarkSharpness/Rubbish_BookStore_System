90fb5c935505713cfc7beea36ca1a879a5dca5b7
DarkSharpness_XzY <2040703891@qq.com>
2022-12-04 15:37:38 +0800

    Finish Simple String Type.

--- include/string.hpp
+++ include/string.hpp
@@ -8,9 +8,10 @@
 namespace dark {
 
 
+template <unsigned len>
 class string {
   private:
-    static constexpr int len = 24;
+    // static constexpr int len = 24;
     char str[len];
  
   public:
@@ -19,13 +20,18 @@ class string {
 
     inline char *begin() {return str;}
     inline const char *begin() const{return str;}
+    inline const char *cbegin() const{return str;}
 
     inline char *end() {return str + len;}
     inline const char *end() const{return str + len;}
+    inline const char *cend() const{return str + len;}
 
-
-
-
+    inline bool operator <(const string &rhs) const{return strcmp(str,rhs.str) < 0;}
+    inline bool operator >(const string &rhs) const{return strcmp(str,rhs.str) > 0;}
+    inline bool operator <=(const string &rhs)const{return strcmp(str,rhs.str) <= 0;}
+    inline bool operator >=(const string &rhs)const{return strcmp(str,rhs.str) >= 0;}
+    inline bool operator ==(const string &rhs)const{return strcmp(str,rhs.str) == 0;}
+    inline bool operator !=(const string &rhs)const{return strcmp(str,rhs.str) != 0;}
 
 
 
