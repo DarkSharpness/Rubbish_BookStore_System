f603c8aae15780f5f5a9bb305419f60561d8b6fd
DarkSharpness_XzY <2040703891@qq.com>
2022-12-29 23:38:30 +0800

    Upd: Simplify Main function

--- main.cpp
+++ main.cpp
@@ -4,12 +4,6 @@
 signed main() {
     std::filesystem::create_directory("bin");
     dark::commandManager Conless; // I love Conless!
-    // freopen("bookstore-testcases\\complex\\testcase4\\1.in","r",stdin);
-    // freopen("bookstore-testcases\\complex\\testcase4\\0.ans","w",stdout);
-    try {
-        while(Conless.runCommand());
-    } catch (const char *str) {
-        std::cout << str;
-    }
+    while(Conless.runCommand());
     return 0;
 }
