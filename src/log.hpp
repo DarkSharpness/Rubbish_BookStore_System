eb24df6389e71821089001be05966f31c84e48a9
DarkSharpness_XzY <2040703891@qq.com>
2023-01-01 17:00:53 +0800

    Upd: Complete LogWriter part.

--- src/log.hpp
+++ src/log.hpp
@@ -10,14 +10,93 @@
 namespace dark {
 
 /* Custom log writer. */
-class LogWriter : private File{
+class LogWriter : private File {
   private:
+    File logInfo;
+    File tradeInfo;
+    std::string buffer;
+    
+    /* Write a char string into buffer. */
+    void writeInfo(const char *msg) {
+        buffer += msg;
+    }
+
+    /* Write a char into buffer. */
+    void writeInfo(char __ch) {
+        buffer += __ch;
+    }
+
+    /* Clear the buffer and write the info by __F. */
+    void writeInfo(File &__F) {
+        buffer += '\n';
+        __F.write(buffer.front(),buffer.size());
+        buffer.clear();
+    }
+
+    /* End of expansion with a new line.*/
+    void writeLog() {return writeInfo(logInfo);}
+
+    /* End of expansion with a new line.*/
+    void writeTrade() {return writeInfo(tradeInfo);}
+
+    /* Write infos in the File. */
+    void writeFile(File &__F) {
+        size_t count = __F.tellp();
+        __F.seekg(0);
+
+        buffer.resize(count);
+        __F.read(buffer.front(),count);
+
+        std::cout << buffer;
+        buffer.clear();
+
+        __F.seekp(0,std::ios::end);
+    }
+
   public:
-    LogWriter(): File("bin/hastin.log") { // I love hastin.
+    LogWriter(): 
+        File("bin/Hastin.log"),
+        logInfo("bin/Conless.log"),
+        tradeInfo("bin/Dark.log"){ // I love them!.
         if(create()) {
             size_t count = 0;
             write(count);
         }
+        logInfo.create();
+        logInfo.seekp(0,std::ios::end);
+        tradeInfo.create();
+        tradeInfo.seekp(0,std::ios::end);
+    }
+
+    /* Write a line of log. */
+    template <class T,class ...V>
+    void writeLog(const T &msg,const V &...msgs) {
+        writeInfo(msg);
+        writeInfo(' ');
+        return writeLog(msgs...);
+    }
+
+    
+    /* Write a line of trade. */
+    template <class T,class ...V>
+    void writeTrade(const T &msg,const V &...msgs) {
+        writeInfo(msg);
+        writeInfo(' ');
+        return writeTrade(msgs...);
+    }
+
+
+    /* Show the inner log. */
+    void showLog() {
+        writeFile(logInfo);
+
+        size_t count;
+        seekg(0);
+        read(count);
+        std::cout << "The following part is about finance:\n"
+                  << "Count of trades:" << count << '\n';
+
+        writeFile(tradeInfo);
     }
 
     /* Write income and outcome. */
